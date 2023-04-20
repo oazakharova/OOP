faf77a1c5fe380eabd01a8dce2c07d516b7611bb
Olga <o.nikolaichenko@yandex.ru>
2023-04-20 13:27:14 +0300

    Init commit

---
+++ OOP/lesson1/src/main/java/ru/geekbrains/BottleOfMilk.java
@@ -0,0 +1,24 @@
+package ru.geekbrains;
+
+public class BottleOfMilk extends Product {
+
+    private int volume;
+    private int fat;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public BottleOfMilk(String name, double price, int volume, int fat){
+        super(name, price);
+        this.volume = volume;
+        this.fat = fat;
+    }
+
+    @Override
+    String displayInfo() {
+        //return super.displayInfo();
+        return String.format("%s - %s - %f - объем: %d", brand, name, price, volume);
+
+    }
+}
---
+++ OOP/lesson1/src/main/java/ru/geekbrains/Main.java
@@ -0,0 +1,58 @@
+package ru.geekbrains;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product();
+        System.out.println(product1.displayInfo());
+
+        Product product2 = new Product("Бутылка с молоком", 200);
+        System.out.println(product2.displayInfo());
+
+        Product product3 = new Product("Ок", "11", -100);
+        System.out.println(product3.displayInfo());
+
+        //product3.setPrice(-50.5);
+       //product3.name = "____";
+       // product3.price = -50.5;
+
+        System.out.println(product3.displayInfo());
+
+        BottleOfWater bottleOfWater1 = new BottleOfWater("Вода", 100, 2);
+        System.out.println(bottleOfWater1.displayInfo());
+
+        BottleOfWater bottleOfWater2 = new BottleOfWater("Вода1", 100, 2);
+        BottleOfWater bottleOfWater3 = new BottleOfWater("Вода2", 110, 1);
+        BottleOfWater bottleOfWater4 = new BottleOfWater("Вода3", 130, 3);
+        BottleOfWater bottleOfWater5 = new BottleOfWater("Вода4", 100, 1);
+
+        Product bottleOfMilk1 = new BottleOfMilk("Молоко", 100, 2, 10);
+
+        List<Product> products = new ArrayList<>();
+        products.add(bottleOfWater2);
+        products.add(bottleOfMilk1);
+        products.add(bottleOfWater3);
+        products.add(bottleOfWater4);
+        products.add(bottleOfWater5);
+
+
+        BottleOfWaterVendingMachine machine = new BottleOfWaterVendingMachine(products);
+
+        BottleOfWater bottleOfWaterRes =  machine.getBottleOfWater("Вода3", 3);
+        if (bottleOfWaterRes != null){
+            System.out.println("Вы купили: ");
+            System.out.println(bottleOfWaterRes.displayInfo());
+        }
+        else {
+            System.out.println("Такой бутылки с водой нет в автомате.");
+        }
+
+    }
+
+
+}
